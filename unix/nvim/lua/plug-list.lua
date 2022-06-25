0b6e5441f9a2ab155921f29e5d0d783113f1592e
Valeriy Sagatovskiy <sagatovskiyva@m2.ru>
2022-06-25 11:50:29 +0300

    removed obsole async/await lib and replaced with local

--- unix/nvim/lua/plug-list.lua
+++ unix/nvim/lua/plug-list.lua
@@ -1,33 +1,33 @@
 return {
-	"wbthomason/packer.nvim",
-	"stevearc/dressing.nvim",
-	"windwp/nvim-autopairs",
+	'wbthomason/packer.nvim',
+	'stevearc/dressing.nvim',
+	'windwp/nvim-autopairs',
 
 	-- Buffer preserver
-	"moll/vim-bbye",
+	'moll/vim-bbye',
 
 	-- Git integration
-	"tpope/vim-fugitive",
-	"tpope/vim-unimpaired",
-	"tpope/vim-dispatch",
-	"shumphrey/fugitive-gitlab.vim",
-	"ms-jpq/lua-async-await",
-	"rbong/vim-flog",
+	'tpope/vim-fugitive',
+	'tpope/vim-unimpaired',
+	'tpope/vim-dispatch',
+	'shumphrey/fugitive-gitlab.vim',
+	'rbong/vim-flog',
+	'junegunn/gv.vim',
 
 	-- Color theme
-	"shaunsingh/nord.nvim",
+	'shaunsingh/nord.nvim',
 
 	-- Syntax highlighting
 	{
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate',
 	},
 
 	-- File explorer
 	{
-		"kyazdani42/nvim-tree.lua",
+		'kyazdani42/nvim-tree.lua',
 		requires = {
-			"kyazdani42/nvim-web-devicons",
+			'kyazdani42/nvim-web-devicons',
 		},
 	},
 }
