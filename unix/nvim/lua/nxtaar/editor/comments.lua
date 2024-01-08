1273dbafd6125d8073e3b8cc8bfc36265c3f3901
Сагатовский Валерий <sagatovskii@tochka.com>
2024-01-08 21:00:53 +0300

    removed ts_comment_string deprecation

--- unix/nvim/lua/nxtaar/editor/comments.lua
+++ unix/nvim/lua/nxtaar/editor/comments.lua
@@ -1,3 +1,5 @@
 local comments = require('nvim_comment')
+vim.g.skip_ts_context_commentstring_module = true
 
 comments.setup({ comment_empty = false })
+require('ts_context_commentstring').setup {}
